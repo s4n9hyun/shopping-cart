77ca995de5965f3ef3be810ae3b4318872542ad9
s4n9hyun <s4n9hyun@gmail.com>
2024-09-11 19:37:21 +0900

    next 과제2

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,56 +1,37 @@
 export const PAGE = {
-  HOME: "/",
-  PRODUCT: "/product",
-  CART: "/cart",
+    HOME: "/",
+    PRODUCT: "/product",
+    CART: "/cart",
 };
 
 export const GNB_TYPE = {
-  MAIN: "MAIN",
-  BACK: "BACK",
+    MAIN: "MAIN",
+    BACK: "BACK",
 };
 
 export const PRODUCTS = [
-  // 상품 목록 추가
-  {
-    id: 0,
-    name: "김치찌개",
-    description:
-      "맛잇는 김치찌개 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
-  {
-    id: 1,
-    name: "된장찌개",
-    description:
-      "맛잇는 된장찌개 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
-  {
-    id: 2,
-    name: "부대찌개",
-    description:
-      "맛잇는 부대찌개 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
-  {
-    id: 3,
-    name: "비빔밥",
-    description:
-      "맛잇는 비빔밥 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
-  {
-    id: 4,
-    name: "떡볶이",
-    description:
-      "맛잇는 떡볶이 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
-  {
-    id: 5,
-    name: "라볶이",
-    description:
-      "맛잇는 라볶이 간단하게 만들어보세요. 신라 호텔 메인 셰프의 레시피로 만들었습니다.",
-    price: 10000,
-  },
+    {
+        id: 1,
+        name: "원룸 (성북구 안암동2가)",
+        description: "17m², 3층, 초역세권, 성북천, 신축",
+        price: "1,000/50",
+    },
+    {
+        id: 2,
+        name: "원룸 (성북구 삼선동2가)",
+        description: "19m², 3층, 한성대입구, 미.친.채.광",
+        price: "1,000/45",
+    },
+    {
+        id: 3,
+        name: "원룸 (동대문구 제기동, 23.1m²)",
+        description: "23.1m², 5층, 풀옵션, 편리한 오픈형 원룸",
+        price: "1,000/65",
+    },
+    {
+        id: 4,
+        name: "원룸 (동대문구 제기동, 24m²)",
+        description: "24m², 4층, Winner, 대로변 오피스텔, 보안",
+        price: "1,000/65",
+    },
 ];
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,51 +4,36 @@ import { GNB } from "components/GNB";
 import { GNB_TYPE } from "constants/common";
 import { ProductInCart } from "components/ProductInCart";
 import { Box } from "styles/StyleComponent";
+import { useCartStore } from "store/CartStore";
 
-function CartPage({ cart, setCart }) {
-  return (
-    <Base>
-      <GNB type={GNB_TYPE.MAIN} />
-      <Inner>
-        <Box gap={30}>
-          {!cart || cart.length <= 0 ? (
-            <Text>등록된 상품이 없습니다.</Text>
-          ) : (
-            cart.map((product, id) => (
-              <ProductInCart
-                key={id}
-                product={product}
-                cart={cart}
-                setCart={setCart}
-              />
-            ))
-          )}
-        </Box>
-      </Inner>
-    </Base>
-  );
+function CartPage() {
+    const { cart, setCart } = useCartStore();
+
+    console.log(cart);
+
+    return (
+        <Base>
+            <GNB type={GNB_TYPE.MAIN} />
+            <Inner>
+                <Box gap={30}>
+                    {cart.map((product, id) => (
+                        <ProductInCart key={id} product={product} cart={cart} setCart={setCart} />
+                    ))}
+                </Box>
+            </Inner>
+        </Base>
+    );
 }
 
 export default CartPage;
 
 const Base = styled.div`
-  width: 100%;
+    width: 100%;
 `;
 const Inner = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 72px 20px 69px;
-`;
-const Text = styled.div`
-  font-family: "Pretendard Variable", sans-serif;
-  font-size: 20px;
-  font-weight: 550;
-  line-height: 135%;
-  text-align: center;
-  color: #717171;
-
-  width: 100%;
-  margin-top: 60px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 72px 20px 69px;
 `;
